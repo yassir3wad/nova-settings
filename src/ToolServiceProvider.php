1c4882b65131741fdaf4b2d90d97d30a30fba6a4
yassirAwad <com.eng.yassir@gmail.com>
2019-08-09 19:21:48 +0300

    first commit

---
+++ src/ToolServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Yassir3wad\Settings;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Yassir3wad\Settings\Http\Middleware\Authorize;
+
+class ToolServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'settings');
+
+        $this->app->booted(function () {
+            $this->routes();
+        });
+
+        $this->loadMigrations();
+    }
+
+    /**
+     * Register the tool's routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        if ($this->app->routesAreCached()) {
+            return;
+        }
+
+        Route::middleware(['nova', Authorize::class])
+                ->prefix('nova-vendor/settings')
+                ->group(__DIR__.'/../routes/api.php');
+    }
+
+    private function loadMigrations()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
